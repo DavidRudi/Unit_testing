2ec9a44e3524316f4df4e6c7327b9f302deff5c2
SelmaZakia <annisa.s.zakia@falinwa.com>
2021-06-16 13:30:17 +0700

    [UPD] indentation error

--- fal_snailmail_lines/tests/snailmail_test.py
+++ fal_snailmail_lines/tests/snailmail_test.py
@@ -6,24 +6,22 @@ _logger = logging.getLogger(__name__)
 
 @tagged('cluedoo')
 class SnailmailLinesTest(TestPingenSend):
-	def setUp(self):
-		is_merge = True
-		invoices = []
-		for i in range(2):
-			invoice = []
-	        invoices.pingen_url = "https://stage-api.pingen.com/document/upload/token/30fc3947dbea4792eb12548b41ec8117/"
-	        invoices.sample_invoice = self.create_invoice()
-	        invoices.sample_invoice.partner_id.vat = "BE000000000"
-        invoices.append(invoice)
+    def setUp(self):
+        invoices = []
+        for i in range(2):
+            invoice = []
+            invoices.pingen_url = "https://stage-api.pingen.com/document/upload/token/30fc3947dbea4792eb12548b41ec8117/"
+            invoices.sample_invoice = self.create_invoice()
+            invoices.sample_invoice.partner_id.vat = "BE000000000"
+            invoices.append(invoice)
 
         self.letter = self.env['account.invoice.send'].create({
-        	'invoice_ids': invoices,
-        	'is_merge': True,
-        	'snailmail_is_letter': True,
-        	'is_email': False,
-        	'is_print': False,
-        	})
-
+            'invoice_ids': invoices,
+            'is_merge': True,
+            'snailmail_is_letter': True,
+            'is_email': False,
+            'is_print': False,
+            })
 
         self.send_and_print_action()
 
