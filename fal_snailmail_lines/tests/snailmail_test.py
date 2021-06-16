6fa244b9484634847652e926423e4b027e5559dd
SelmaZakia <annisa.s.zakia@falinwa.com>
2021-06-16 16:07:53 +0700

    [UPD] fal_snailmail_lines

--- fal_snailmail_lines/tests/snailmail_test.py
+++ fal_snailmail_lines/tests/snailmail_test.py
@@ -10,10 +10,11 @@ class SnailmailLinesTest(TestPingenSend):
         invoices = []
         for i in range(2):
             invoice = []
-            invoices.pingen_url = "https://stage-api.pingen.com/document/upload/token/30fc3947dbea4792eb12548b41ec8117/"
-            invoices.sample_invoice = self.create_invoice()
-            invoices.sample_invoice.partner_id.vat = "BE000000000"
+            invoice.pingen_url = "https://stage-api.pingen.com/document/upload/token/30fc3947dbea4792eb12548b41ec8117/"
+            invoice.sample_invoice = self.create_invoice()
+            invoice.sample_invoice.partner_id.vat = "BE000000000"
             invoices.append(invoice)
+            _logger.info(invoices.id,"ID Invoice")
 
         self.letter = self.env['account.invoice.send'].create({
             'invoice_ids': invoices,
@@ -25,5 +26,13 @@ class SnailmailLinesTest(TestPingenSend):
 
         self.send_and_print_action()
 
+        temp=[]
+        for invoice in invoices:
+            temp.append(invoice.id)
 
-        
+        snailmail = self.env['snailmail.letter']
+        if (snailmail.search([('is_snailmail_merge','=',True)])):
+            self.assertEquals(2, len(snailmail_records))
+            for doc in range(len(invoices)):
+                self.assertEquals(snailmail.snailmail_records[doc].document_id, temp[doc])
+                self.assertEquals(snailmail.snailmail_records[doc].model_id, 'account.move')
